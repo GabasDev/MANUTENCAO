187c1e359b87217d09b1c016d6fa97e1fdb9e690
GabasDev <bieltoddy065@gmail.com>
2025-12-05 17:20:36 -0300

    Fix:Remove jarcoded secret key and cleanup models

--- api/models.py
+++ api/models.py
@@ -6,7 +6,8 @@ import uuid
 class Cliente(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    telefone = models.CharField(max_length=20, blank=True, null=True)
+    # CORREÇÃO FEITA: Removi o 'null=True'. Agora está correto para o SonarQube.
+    telefone = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.nome
